e4b4788c0fef126f1c2a205b0505a83cc76cfe40
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2025-05-17 02:03:56 +0530

    backend added

---
+++ backend/Controllers/userController.js
@@ -0,0 +1,163 @@
+// import UserSchema from "../models/UserSchema.js";
+
+// export const updateUser = async (req,res)=>{
+//     const id = req.params.id
+//     try {
+//         const updatedUser = await User.findByIdAndUpdate(id, { $set: req.body }, { new: true })
+
+//         res.status(200).json({
+//             success:true,
+//             message:"Successfully updated",
+//             data:updatedUser,
+//         })
+//     } catch (error) {
+//         res.status(500).json({
+//             success:false,
+//             message:"Failed to update",
+//         })
+//     }
+// }
+
+// export const deleteUser = async (req,res)=>{
+//     const id = req.params.id
+//     try {
+//         await User.findByIdAndDelete(
+//             id,
+//         )
+
+//         res.status(200).json({
+//             success:true,
+//             message:"Successfully deleted",
+//         })
+//     } catch (error) {
+//         res.status(500).json({
+//             success:false,
+//             message:"Failed to delete",
+//         })
+//     }
+// }
+
+// export const getSingleUser = async (req,res)=>{
+//     const id = req.params.id
+//     try {
+//         const user = await User.findById(id,)
+
+//         res.status(200).json({
+//             success:true,
+//             message:"User found",
+//             data:user,
+//         })
+//     } catch (error) {
+//         res.status(404).json({
+//             success:false,
+//             message:"No user found",
+//         })
+//     }
+// }
+
+// export const getAllUser = async (req,res)=>{
+//     try {
+//         const user = await User.find({});
+
+//         res.status(200).json({
+//             success:true,
+//             message:"Users found",
+//             data:users,
+//         })
+//     } catch (error) {
+//         res.status(404).json({
+//             success:false,
+//             message:"Not found",
+//         })
+//     }
+// }
+
+import User from "../models/UserSchema.js";
+
+export const updateUser = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Successfully updated",
+      data: updatedUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to update!",
+      data: updateUser,
+    });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    await User.findByIdAndDelete(id);
+
+    res.status(200).json({
+      success: true,
+      message: "Successfully deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to delete!",
+      data: updateUser,
+    });
+  }
+};
+
+export const getSingleUser = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const user = await User.findById(id).select("-password"); // Query the user by ID
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "No User Found!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User Found",
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to retrieve user!",
+      data: error,
+    });
+  }
+};
+
+export const getAllUser = async (req, res) => {
+  try {
+    const users = await User.find().select("-password"); // exclude password from appearing
+
+    res.status(200).json({
+      success: true,
+      message: "Users Found",
+      data: users,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to retrieve users!",
+      data: error.message,
+    });
+  }
+};
---
+++ backend/index.js
@@ -0,0 +1,52 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import authRoute from './Routes/auth.js'
+import userRoute from './Routes/user.js'
+import doctorRoute from './Routes/doctor.js'
+import reviewRoute from './Routes/review.js'
+
+dotenv.config()
+
+const app=express()
+
+const port=process.env.PORT || 8000
+
+const corsOptions={
+    origin:"http://localhost:5173",
+    credentials: true
+}
+
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+    res.send('API is working')
+})
+
+mongoose.set('strictQuery',false)
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Database connected");
+    } catch (err) {
+        console.log('Database connection failed:', err.message);
+    }
+}
+
+app.use(express.json())
+app.use(cookieParser())
+app.use('/api/v1/doctors', doctorRoute)
+app.use('/api/v1/reviews', reviewRoute)
+app.use('/api/v1/users', userRoute)
+app.use('/api/v1/auth', authRoute);
+
+// app.use('/api/v1/doctors', doctorRoute)
+// app.use('api/v1/reviews', reviewRoute)
+
+app.listen(port,()=>{
+    connectDB();
+    console.log(`Server is running on port ${port}`)
+})
+
