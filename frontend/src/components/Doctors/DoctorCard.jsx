53c6b886a8dd9a84698cbac2a980193c75c26b0d
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2025-05-19 03:16:00 +0530

    Doctors component changes

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -11,8 +11,8 @@ function DoctorCard({doctor}) {
     avgRating ,
     totalRating , 
     photo , 
-    totalPatients ,
-    hospital }
+    experiences
+     }
       = doctor
 
   return (
@@ -41,17 +41,17 @@ function DoctorCard({doctor}) {
 
           <div className='mt-[18px] lg:mt-5 flex items-center justify-between'>
             <div>
-              <h3 className='text-[16px] leading-7 lg:text-[18px] lg:leading-[30px] font-semibold
+              {/* <h3 className='text-[16px] leading-7 lg:text-[18px] lg:leading-[30px] font-semibold
                text-headingColor'>
                 + {totalPatients} Patients
-              </h3>
+              </h3> */}
               <p className='text-[14px] leading-6 font-[400]
               text-textColor'> 
-                At {hospital}
+                At {experiences && experiences[0]?.hospital}
               </p>
             </div>
 
-            <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E]
+            <Link to={`/doctors/${doctor._id}`} className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E]
                    flex items-center group hover:bg-primaryColor hover:border-none justify-center'>
               <BsArrowRight className='group-hover:text-white w-6 h-5'/>
             </Link>
--- frontend/src/components/Doctors/DoctorList.jsx
+++ frontend/src/components/Doctors/DoctorList.jsx
@@ -1,15 +1,25 @@
-import { doctors } from './../../assets/data/doctors'
 import DoctorCard from './DoctorCard'
-
+import { BASE_URL } from '../../../config'
+import useFetchData from '../../hooks/useFetchData'
+import Loading from '../Loader/Loading'
+import Error from '../Error/Error'
 
 function DoctorList() {
+  const {data:doctors, loading, error}= useFetchData(`${BASE_URL}/doctors`)
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] 
-     lg:mt-[55px]'> 
-        {doctors.map (doctor => (
-          <DoctorCard key={doctor.id} doctor={doctor} />
-        ))}
-    </div>
+    <>
+    {loading && <Loading/>}
+
+    {error && <Error/>}
+
+    {!loading && !error && <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] 
+      lg:mt-[55px]'> 
+          {doctors.map (doctor => (
+            <DoctorCard key={doctor._id} doctor={doctor} />
+          ))}
+      </div>}
+      
+    </>
   )
 }
 
