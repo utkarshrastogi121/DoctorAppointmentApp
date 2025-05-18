64485cbac5174fc93c394300dac25ee38f9ec24f
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2025-05-19 01:42:57 +0530

    few additions in DoctorAbout page

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import { formatDate } from '../../utils/formatDate'
 
 const DoctorAbout = ({ name, about, qualifications, experiences }) => {
@@ -8,11 +7,11 @@ const DoctorAbout = ({ name, about, qualifications, experiences }) => {
             <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2'>
                 About Of
                 <span className='text-irisBlueColor font-bold text-[24px] leading-9'>
-                    Utkarsh
+                    {name}
                 </span>
             </h3>
             <p className="text__para">
-                Dr. Utkarsh is a highly skilled and dedicated surgeon with extensive experience in both academic and clinical settings. He is committed to providing exceptional patient care and advancing medical knowledge through continuous learning and research. His expertise spans complex surgical procedures, and he is known for his compassionate approach and professionalism.
+                {about}
             </p>
         </div>
 
@@ -21,19 +20,22 @@ const DoctorAbout = ({ name, about, qualifications, experiences }) => {
                 Education
             </h3>
             <ul className='pt-4 md:p-5'>
-                <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
+
+                {qualifications?.map((item,index)=> <li key={index} className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
                     <div>
                         <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>
-                        {formatDate('06-04-2010')} - {formatDate('12-04-2017')}
+                        {formatDate(item.startingDate)} - {formatDate(item.endingDate)}
                         </span>
                         <p className='text-[16px] leading-6 font-medium text-textColor'>
-                            PhD in Surgeon
+                            {item.degree}
                         </p>
                     </div>
                     <p className='text-[14px] leading-5 font-medium text-textColor'>
-                            FMSo, Sousse.
+                        {item.university}
                     </p>
-                </li>
+                </li>)}
+
+                
                 <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
                     <div>
                         <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>
@@ -55,17 +57,19 @@ const DoctorAbout = ({ name, about, qualifications, experiences }) => {
                 Experience
             </h3>
             <ul className='grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5'>
-                <li className='p-4 rounded bg-[#fff9ea]'>
+
+                {experiences?.map((item, index)=> <li key={index} className='p-4 rounded bg-[#fff9ea]'>
                     <span className='text-yellowColor text-[15px] leading-6 font-semibold'>
-                    {formatDate('12-04-2018')} - {formatDate('12-04-2020')}
+                    {formatDate(item.startingDate)} - {formatDate(item.endingDate)}
                     </span>
                     <p className='text-[16px] leading-6 font-medium text-textColor'>
-                            Surgeon Doctor
+                        {item.position}
                     </p>
                     <p className='text-[16px] leading-6 font-medium text-textColor'>
-                            Fattouma Bourgiba, Monastir,
+                        {item.hospital}
                     </p>
-                </li>
+                </li>)}
+                
                 <li className='p-4 rounded bg-[#fff9ea]'>
                     <span className='text-yellowColor text-[15px] leading-6 font-semibold'>
                     {formatDate('12-04-2020')} - {formatDate('12-04-2022')}
