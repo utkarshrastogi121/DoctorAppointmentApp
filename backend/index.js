4ddc586c8d1e0692801e5138a86ba9607347fe01
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2025-06-03 15:28:53 +0530

    changes for deployemnt

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app=express()
 const port=process.env.PORT 
 
 const corsOptions={
-    origin:"http://localhost:5173",
+    origin:"https://doctorappointment2-1fef.onrender.com",
     credentials: true
 }
 
