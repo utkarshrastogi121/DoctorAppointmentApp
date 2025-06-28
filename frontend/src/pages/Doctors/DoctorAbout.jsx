be7e9c2ec30cfa08ee4dec077bf99f791448f351
Utkarsh Rastogi <145201743+utkarshrastogi121@users.noreply.github.com>
2025-06-28 18:32:22 +0530

    Update DoctorAbout.jsx

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,90 +1,67 @@
-import { formatDate } from '../../utils/formatDate'
+import { formatDate } from '../../utils/formatDate';
 
 const DoctorAbout = ({ name, about, qualifications, experiences }) => {
   return (
     <div>
-        <div>
-            <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2'>
-                About Of
-                <span className='text-irisBlueColor font-bold text-[24px] leading-9'>
-                    {name}
-                </span>
-            </h3>
-            <p className="text__para">
-                {about}
-            </p>
-        </div>
+      <div>
+        <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2'>
+          About Of
+          <span className='text-irisBlueColor font-bold text-[24px] leading-9'>
+            {name}
+          </span>
+        </h3>
+        <p className="text__para">{about}</p>
+      </div>
 
+      {qualifications?.length > 0 && (
         <div className='mt-12'>
-            <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold'>
-                Education
-            </h3>
-            <ul className='pt-4 md:p-5'>
-
-                {qualifications?.map((item,index)=> <li key={index} className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
-                    <div>
-                        <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>
-                        {formatDate(item.startingDate)} - {formatDate(item.endingDate)}
-                        </span>
-                        <p className='text-[16px] leading-6 font-medium text-textColor'>
-                            {item.degree}
-                        </p>
-                    </div>
-                    <p className='text-[14px] leading-5 font-medium text-textColor'>
-                        {item.university}
-                    </p>
-                </li>)}
-
-                
-                <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
-                    <div>
-                        <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>
-                            {formatDate('12-04-2017')} - {formatDate('12-04-2028')}
-                        </span>
-                        <p className='text-[16px] leading-6 font-medium text-textColor'>
-                            Internships
-                        </p>
-                    </div>
-                    <p className='text-[14px] leading-5 font-medium text-textColor'>
-                            Charles Nicol Hospital, Tunis.
-                    </p>
-                </li>
-            </ul>
-        </div>
-
-        <div className='mt-12'>
-            <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold'>
-                Experience
-            </h3>
-            <ul className='grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5'>
-
-                {experiences?.map((item, index)=> <li key={index} className='p-4 rounded bg-[#fff9ea]'>
-                    <span className='text-yellowColor text-[15px] leading-6 font-semibold'>
+          <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold'>
+            Education
+          </h3>
+          <ul className='pt-4 md:p-5'>
+            {qualifications.map((item, index) => (
+              <li key={index} className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
+                <div>
+                  <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>
                     {formatDate(item.startingDate)} - {formatDate(item.endingDate)}
-                    </span>
-                    <p className='text-[16px] leading-6 font-medium text-textColor'>
-                        {item.position}
-                    </p>
-                    <p className='text-[16px] leading-6 font-medium text-textColor'>
-                        {item.hospital}
-                    </p>
-                </li>)}
-                
-                <li className='p-4 rounded bg-[#fff9ea]'>
-                    <span className='text-yellowColor text-[15px] leading-6 font-semibold'>
-                    {formatDate('12-04-2020')} - {formatDate('12-04-2022')}
-                    </span>
-                    <p className='text-[16px] leading-6 font-medium text-textColor'>
-                            Surgeon Doctor
-                    </p>
-                    <p className='text-[16px] leading-6 font-medium text-textColor'>
-                            Fattouma Bourgiba, Monastir,
-                    </p>
-                </li>
-            </ul>
+                  </span>
+                  <p className='text-[16px] leading-6 font-medium text-textColor'>
+                    {item.degree}
+                  </p>
+                </div>
+                <p className='text-[14px] leading-5 font-medium text-textColor'>
+                  {item.university}
+                </p>
+              </li>
+            ))}
+          </ul>
         </div>
-    </div>
-  )
-}
+      )}
 
-export default DoctorAbout
+      {experiences?.length > 0 && (
+        <div className='mt-12'>
+          <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold'>
+            Experience
+          </h3>
+          <ul className='grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5'>
+            {experiences.map((item, index) => (
+              <li key={index} className='p-4 rounded bg-[#fff9ea]'>
+                <span className='text-yellowColor text-[15px] leading-6 font-semibold'>
+                  {formatDate(item.startingDate)} - {formatDate(item.endingDate)}
+                </span>
+                <p className='text-[16px] leading-6 font-medium text-textColor'>
+                  {item.position}
+                </p>
+                <p className='text-[16px] leading-6 font-medium text-textColor'>
+                  {item.hospital}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DoctorAbout;
