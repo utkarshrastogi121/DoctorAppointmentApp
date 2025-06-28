f42e2714c6416007d99ee5beb4fa7890d1c0e5f6
Utkarsh Rastogi <145201743+utkarshrastogi121@users.noreply.github.com>
2025-06-28 17:53:57 +0530

    Update SidePanel.jsx

--- frontend/src/pages/Doctors/SidePanel.jsx
+++ frontend/src/pages/Doctors/SidePanel.jsx
@@ -43,10 +43,10 @@ const SidePanel = ({doctorId, ticketPrice, timeSlots}) => {
                 {timeSlots?.map((item, index)=>(
                     <li key={index} className='flex items-center justify-between mb-2'>
                     <p className='text-[15px] leading-6 text-textColor font-semibold'>
-                        {item.day.charAt(0).toUppercase() + item.day.slice(1)}
+                        {item.day.charAt(0).toUpperCase() + item.day.slice(1)}
                     </p>
                     <p className='text-[15px] leading-6 text-textColor font-semibold'>
-                        {convertTime(item.startngTime)} - {convertTime(item.endingTime)}
+                        {convertTime(item.startingTime)} - {convertTime(item.endingTime)}
                     </p>
                 </li>
                 ))}
