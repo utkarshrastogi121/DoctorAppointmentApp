65884e28ab909c6b4c1a63c74c83da6811c15b77
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2026-02-03 23:13:58 +0530

    changes in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,26 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { AuthContextProvider } from './context/AuthContext.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AuthContextProvider } from './context/AuthContext.jsx';
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
-                <App/>
+        <App />
         <ToastContainer
-        theme="dark"
-        position="top-right"
-        autoClose={3000}
-        closeOnClick
-        pauseOnHover={false}
-        style={{ zIndex: 9999 }}
-      />
+          theme="dark"
+          position="top-right"
+          autoClose={3000}
+          closeOnClick
+          pauseOnHover={false}
+          style={{ zIndex: 9999 }}
+        />
       </AuthContextProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
