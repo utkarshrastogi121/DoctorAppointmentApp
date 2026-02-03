7a553bef213ba678588f1047e810feb468c279cc
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2026-02-03 22:56:23 +0530

    Doctor card changes

--- frontend/src/components/Doctors/DoctorList.jsx
+++ frontend/src/components/Doctors/DoctorList.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import DoctorCard from './DoctorCard'
 import { BASE_URL } from '../../../config'
 import useFetchData from '../../hooks/useFetchData'
@@ -5,21 +6,19 @@ import Loading from '../Loader/Loading'
 import Error from '../Error/Error'
 
 function DoctorList() {
-  const {data:doctors, loading, error}= useFetchData(`${BASE_URL}/doctors`)
+  const { data: doctors, loading, error } = useFetchData(`${BASE_URL}/doctors`)
+
+  if (loading) return <Loading />
+  if (error) return <Error />
+  if (!doctors || doctors.length === 0)
+    return <p className="text-center text-textColor mt-10">No doctors found.</p>
+
   return (
-    <>
-    {loading && <Loading/>}
-
-    {error && <Error/>}
-
-    {!loading && !error && <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] 
-      lg:mt-[55px]'> 
-          {doctors.map (doctor => (
-            <DoctorCard key={doctor._id} doctor={doctor} />
-          ))}
-      </div>}
-      
-    </>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
+      {doctors.map(doctor => (
+        <DoctorCard key={doctor._id} doctor={doctor} />
+      ))}
+    </div>
   )
 }
 
