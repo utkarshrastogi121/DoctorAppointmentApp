58e6db35d257831568c40ab89d8f7a549e9dd4d4
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2026-02-03 23:00:08 +0530

    Service card updated

--- frontend/src/components/Services/ServiceCard.jsx
+++ frontend/src/components/Services/ServiceCard.jsx
@@ -5,14 +5,18 @@ function ServiceCard({ item, index }) {
   const { name, desc, bgColor, textColor } = item;
 
   return (
-    <div className="py-[30px] px-3 lg:px-5">
-      <h2 className="text-[26px] leading-9 text-headingColor font-[700]">
-        {name}
-      </h2>
-      <p className="text-[16px] leading-7 font-[400] text-textColor mt-4">
-        {desc}
-      </p>
+    <div className="py-[30px] px-3 lg:px-5 bg-white rounded-lg shadow-md flex flex-col justify-between h-full">
+      {/* Content */}
+      <div>
+        <h2 className="text-[26px] leading-9 text-headingColor font-[700]">
+          {name}
+        </h2>
+        <p className="text-[16px] leading-7 font-[400] text-textColor mt-4">
+          {desc}
+        </p>
+      </div>
 
+      {/* Footer */}
       <div className="flex items-center justify-between mt-[30px]">
         <Link
           to="/doctors"
