03c781a1eedb34130d61edc2932e6fb80859eb1e
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2026-02-04 23:19:22 +0530

    index.js changes

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,6 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import path from "path"
-import { fileURLToPath } from "url"
 import authRoute from './Routes/auth.js'
 import userRoute from './Routes/user.js'
 import doctorRoute from './Routes/doctor.js'
@@ -13,32 +11,14 @@ import bookingRoute from './Routes/booking.js'
 
 dotenv.config()
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
 const app=express()
 
 const port=process.env.PORT 
 
 app.use(cors());
-app.use(express.json())
-app.use(cookieParser())
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.send('API is working')
-})
-
-
-app.use('/api/v1/doctors', doctorRoute)
-app.use('/api/v1/reviews', reviewRoute)
-app.use('/api/v1/users', userRoute)
-app.use('/api/v1/auth', authRoute);
-app.use('/api/v1/bookings', bookingRoute);
-
-app.use(express.static(path.join(__dirname, "dist")))
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
 
 mongoose.set('strictQuery',false)
@@ -50,6 +30,14 @@ const connectDB = async () => {
         console.log('Database connection failed:', err.message);
     }
 }
+
+app.use(express.json())
+app.use(cookieParser())
+app.use('/api/v1/doctors', doctorRoute)
+app.use('/api/v1/reviews', reviewRoute)
+app.use('/api/v1/users', userRoute)
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/bookings', bookingRoute);
 
 
 app.listen(port,()=>{
