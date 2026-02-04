e1a6185fc5ef43a0ea6749458a276520704c23cc
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2026-02-04 19:27:57 +0530

    user schema changes

--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, select: false },
   name: { type: String, required: true },
   phone: { type: Number },
   photo: { type: String },
